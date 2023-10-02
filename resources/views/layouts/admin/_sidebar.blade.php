67299d818890691936413909ea9d1dd15c1b8132
Fathurrahman Prasetyo Aji <ajifatur2@gmail.com>
2023-10-02 11:11:31 +0700

    Update _sidebar.blade.php

--- resources/views/layouts/admin/_sidebar.blade.php
+++ resources/views/layouts/admin/_sidebar.blade.php
@@ -9,18 +9,18 @@
 			@endif
 		</a>
 		<ul class="sidebar-nav">
-			@foreach(menu() as $menu)
+			@foreach(menu() as $key=>$menu)
 				@if($menu['header'] != '' && count($menu['items']) > 0)
 					<li class="sidebar-header">{{ $menu['header'] }}</li>
 				@endif
 				@if(count($menu['items']) > 0)
-					@foreach($menu['items'] as $key=>$item)
+					@foreach($menu['items'] as $key2=>$item)
 						@if(count($item['children']) > 0)
 							<li class="sidebar-item {{ eval_sidebar($item['active_conditions'], 'active') }}">
-								<a data-bs-target="#sidebar-subitem-{{ $key }}" data-bs-toggle="collapse" class="sidebar-link {{ eval_sidebar($item['active_conditions'], '', 'collapsed') }}">
+								<a data-bs-target="#sidebar-subitem-{{ $key }}-{{ $key2 }}" data-bs-toggle="collapse" class="sidebar-link {{ eval_sidebar($item['active_conditions'], '', 'collapsed') }}">
 									<i class="align-middle {{ $item['icon'] }}" style="font-size: 1rem;"></i> <span class="align-middle">{{ $item['name'] }}</span>
 								</a>
-								<ul id="sidebar-subitem-{{ $key }}" class="sidebar-dropdown list-unstyled collapse {{ eval_sidebar($item['active_conditions'], 'show') }}" data-bs-parent="#sidebar">
+								<ul id="sidebar-subitem-{{ $key }}-{{ $key2 }}" class="sidebar-dropdown list-unstyled collapse {{ eval_sidebar($item['active_conditions'], 'show') }}" data-bs-parent="#sidebar">
 									@foreach($item['children'] as $subitem)
 									<li class="sidebar-item {{ eval_sidebar($subitem['active_conditions'], 'active') }}"><a class="sidebar-link" href="{{ $subitem['route'] }}">{{ $subitem['name'] }}</a></li>
 									@endforeach
