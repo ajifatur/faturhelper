93274d6510ac1541ea2bb8fc1926a667a0c6c7b2
Fathurrahman Prasetyo Aji <ajifatur2@gmail.com>
2025-12-12 11:36:52 +0700

    Update create.blade.php

--- resources/views/admin/menu-item/create.blade.php
+++ resources/views/admin/menu-item/create.blade.php
@@ -165,7 +165,6 @@
                                 <option value="0">Tidak Ada</option>
                                 @foreach($menu_parents as $menu_parent)
                                 <option value="{{ $menu_parent->id }}" {{ old('parent') == $menu_parent->id ? 'selected' : '' }}>
-                                    {{ ($menu_parent->menu_parent ? $menu_parent->menu_parent->name.' - ' : '') }}
                                     {{ $menu_parent->name }}
                                 </option>
                                 @endforeach
